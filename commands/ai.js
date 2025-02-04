6f77836968af3e231eed2f855d12a2287493b968
Satoru0617 <satorutechnologie@gmail.com>
2025-02-04 22:19:52 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -7,7 +7,7 @@ const token = fs.readFileSync('token.txt', 'utf8');
 const useFontFormatting = true;
 
 module.exports = {
-  name: 'gpt4',
+  name: 'ai',
   description: 'Interact to Free GPT - OpenAI.',
   author: 'Arn', // API by Kenlie Navacilla Jugarap
 
