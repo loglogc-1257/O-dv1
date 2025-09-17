1d60d105279cac2702ae592d2e4025d93ffde419
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-09-17 11:23:02 +0100

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -1,38 +1,36 @@
-const request = require('request');
+const axios = require('axios');
 
 function sendMessage(senderId, message, pageAccessToken) {
-  if (!message || (!message.text && !message.attachment)) {
-    console.error('Error: Message must provide valid text or attachment.');
-    return;
-  }
-
-  const payload = {
-    recipient: { id: senderId },
-    message: {}
-  };
-
-  if (message.text) {
-    payload.message.text = message.text;
-  }
-
-  if (message.attachment) {
-    payload.message.attachment = message.attachment;
-  }
-
-  request({
-    url: 'https://graph.facebook.com/v13.0/me/messages',
-    qs: { access_token: pageAccessToken },
-    method: 'POST',
-    json: payload,
-  }, (error, response, body) => {
-    if (error) {
-      console.error('Error sending message:', error);
-    } else if (response.body.error) {
-      console.error('Error response:', response.body.error);
-    } else {
-      console.log('Message sent successfully:', body);
+    if (!message || (!message.text && !message.attachment)) {
+        console.error('Error: Message must provide valid text or attachment.');
+        return;
     }
-  });
+
+    const url = `https://graph.facebook.com/v19.0/me/messages?access_token=${pageAccessToken}`;
+    const payload = {
+        recipient: {
+            id: senderId,
+        },
+        message: {}
+    };
+
+    if (message.text) {
+        payload.message.text = message.text;
+    }
+
+    if (message.attachment) {
+        payload.message.attachment = message.attachment;
+    }
+
+    axios.post(url, payload)
+        .then(response => {
+            console.log('Message sent successfully:', response.data);
+        })
+        .catch(error => {
+            console.error('Failed to send message:', error.response.data);
+        });
 }
 
-module.exports = { sendMessage };
+module.exports = {
+    sendMessage,
+};
